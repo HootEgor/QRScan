fc5eac5738047d13d22cc2c30391c074820a7d41
ruslanhut <dev@programmer.com.ua>
2022-03-29 15:35:19 +0300

    kotlin conversion

--- app/src/main/java/ua/com/programmer/barcodetest/Utils.kt
+++ app/src/main/java/ua/com/programmer/barcodetest/Utils.kt
@@ -1,63 +1,35 @@
-package ua.com.programmer.barcodetest;
+package ua.com.programmer.barcodetest
 
-import android.util.Log;
+import android.util.Log
+import com.google.mlkit.vision.barcode.common.Barcode
+import java.util.*
 
-import com.google.mlkit.vision.barcode.common.Barcode;
+internal class Utils {
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
-class Utils {
-
-    long dateBeginOfToday(){
-        Calendar calendar = new GregorianCalendar();
-        int currentYear = calendar.get(Calendar.YEAR);
-        int currentMonth = calendar.get(Calendar.MONTH);
-        int currentDay = calendar.get(Calendar.DATE);
-        calendar.set(currentYear,currentMonth,currentDay,0,0);
-        return calendar.getTimeInMillis()/1000;
+    fun dateBeginShiftDate(): Long {
+        val calendar: Calendar = GregorianCalendar()
+        val currentYear = calendar[Calendar.YEAR]
+        val currentMonth = calendar[Calendar.MONTH]
+        val currentDay = calendar[Calendar.DATE]
+        calendar[currentYear, currentMonth, currentDay, 0] = 0
+        return calendar.timeInMillis / 1000 - 86400 * 30
     }
 
-    long dateBeginShiftDate(){
-        Calendar calendar = new GregorianCalendar();
-        int currentYear = calendar.get(Calendar.YEAR);
-        int currentMonth = calendar.get(Calendar.MONTH);
-        int currentDay = calendar.get(Calendar.DATE);
-        calendar.set(currentYear,currentMonth,currentDay,0,0);
-        return calendar.getTimeInMillis()/1000 - 86400*30;
-    }
-
-    String nameOfBarcodeFormat(int format){
-        String name;
-        switch (format) {
-            case Barcode.FORMAT_QR_CODE:
-                name = "QR code";
-                break;
-            case Barcode.FORMAT_DATA_MATRIX:
-                name = "Data Matrix";
-                break;
-            case Barcode.FORMAT_EAN_13:
-                name = "EAN13";
-                break;
-            case Barcode.FORMAT_CODE_128:
-                name = "Code128";
-                break;
-            case Barcode.FORMAT_UPC_A:
-                name = "UPC A";
-                break;
-            case Barcode.TYPE_CALENDAR_EVENT:
-                name = "Calendar event";
-                break;
-            case Barcode.TYPE_URL:
-                name = "URL";
-                break;
-            default:
-                name = "?";
+    fun nameOfBarcodeFormat(format: Int): String {
+        val name: String = when (format) {
+            Barcode.FORMAT_QR_CODE -> "QR code"
+            Barcode.FORMAT_DATA_MATRIX -> "Data Matrix"
+            Barcode.FORMAT_EAN_13 -> "EAN13"
+            Barcode.FORMAT_CODE_128 -> "Code128"
+            Barcode.FORMAT_UPC_A -> "UPC A"
+            Barcode.TYPE_CALENDAR_EVENT -> "Calendar event"
+            Barcode.TYPE_URL -> "URL"
+            else -> "?"
         }
-        return name;
+        return name
     }
 
-    public void debug(String text){
-        Log.d("XBUG", text);
+    fun debug(text: String?) {
+        Log.d("XBUG", text!!)
     }
 }
